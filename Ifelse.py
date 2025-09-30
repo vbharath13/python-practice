f3a91f701fb0f7bb580e1352cd6cf7bafb433fcd
Bharath Vadla <bharathc0513@gmail.com>
2025-09-30 10:14:41 -0400

    if else statements

---
+++ Ifelse.py
@@ -0,0 +1,8 @@
+something = "Python"
+
+if something == "Python":
+    print("Python is an interpreted, high-level, general-purpose programming language.")
+elif something == "bug":
+    print("A bug is a a common term for an error, flaw or fault in a computer program.")
+else:
+    print("I don't know what", something, "means.")
