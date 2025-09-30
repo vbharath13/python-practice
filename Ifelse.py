78adc4de7c7093bb204d1c25a410eb8b88d6089e
Bharath Vadla <bharathc0513@gmail.com>
2025-09-30 10:24:49 -0400

    updated file

--- Ifelse.py
+++ Ifelse.py
@@ -6,3 +6,32 @@ elif something == "bug":
     print("A bug is a a common term for an error, flaw or fault in a computer program.")
 else:
     print("I don't know what", something, "means.")
+
+#ask a word and print
+word = input("Enter a word: ")
+print(word * 1000)
+
+
+#add a space to the word before we print it.
+word = input("Enter a word: ")
+word += " "
+print(word * 1000)
+
+#We can also add the space right away on the first line:
+
+word = input("Enter a word: ") + " "
+print(word * 1000)
+
+
+#this has lot more spaces so we can do something like this instead:
+no_space = input("Enter a word: ")
+yes_space = no_space + " "
+print(yes_space*999 + no_space)
+
+
+ 
+
+
+
+
+
